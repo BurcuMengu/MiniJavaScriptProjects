e4d8cd9f6f2c8d88afde7ef7cb22c50dcafaf259
BurcuMengu <burcumengu@gmail.com>
2025-10-07 19:32:58 +0300

    push

--- Tutorial/index.js
+++ Tutorial/index.js
@@ -9,8 +9,8 @@ if (shouldWePlay.toLowerCase() === "yes") {
     const leftOrRight = prompt("You enter a maze, do you want to go left or right? ");
     if (leftOrRight === "left") {
         console.log("You go left and see a bridge...");
-        const cross = prompt("Do you want to cross the bridge? ");
-        if (cross === "yes") {
+        const cross = prompt("Do you want to cross the bridge? ").toLowerCase();
+        if (cross === "yes" || cross === "y" || cross === "okay") {
             console.log("You cross but the bridge was weak and broke an you fell. You lost...");
         } else {
             console.log("Good choice... You win");
