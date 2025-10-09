c2b3773383c5275d41fc21166a12ce7c5f9af4d8
BurcuMengu <burcumengu@gmail.com>
2025-10-09 23:02:28 +0300

    push

---
+++ TextBasedCalculator/index.js
@@ -0,0 +1,27 @@
+const prompt = require("prompt-sync")();
+
+const number1 = parseFloat(prompt("Enter Number 1: "))
+const number2 = parseFloat(prompt("Enter Number 2: "))
+const operator = prompt("Enter Sign: ")
+
+let result;
+
+switch(operator) {
+    case "+":
+        result = number1 + number2
+        break;
+    case "-":
+        result = number1 - number2
+        break;
+    case "/":
+        result = number1 / number2
+        break;
+    case "*":
+        result = number1 * number2
+        break;
+    default:
+        console.log("Invalid")
+        break;
+}
+
+console.log(result)
