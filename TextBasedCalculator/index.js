7fb91b4b1d891ab5636a58d90ad3f6b91d73822f
BurcuMengu <burcumengu@gmail.com>
2025-10-10 15:03:15 +0300

    push

--- TextBasedCalculator/index.js
+++ TextBasedCalculator/index.js
@@ -1,10 +1,43 @@
 const prompt = require("prompt-sync")();
 
-const number1 = parseFloat(prompt("Enter Number 1: "))
-const number2 = parseFloat(prompt("Enter Number 2: "))
+function getNumber(numberString) {
+    while (true) {
+        const number = parseFloat(prompt("Enter Number" + numberString + ": "))
+        if (isNaN(number)) {
+            console.log("Invalid input")
+        } else {
+            return number
+        }
+    }
+}
+
+const number1 = getNumber("One")
+const number2 = getNumber("Two")
+
+/*let number1;
+let number2;
+while (true) {
+    number1 = parseFloat(prompt("Enter Number 1: "))
+    if (isNaN(number1)) {
+        console.log("Invalid input")
+    } else {
+        break
+    }
+}
+
+while (true) {
+    number2 = parseFloat(prompt("Enter Number 2: "))
+    if (isNaN(number2)) {
+        console.log("Invalid input")
+    } else {
+        break
+    }
+}*/
+
 const operator = prompt("Enter Sign: ")
 
 let result;
+let valid = true;
 
 switch(operator) {
     case "+":
@@ -14,6 +47,10 @@ switch(operator) {
         result = number1 - number2
         break;
     case "/":
+        if (number2 === 0) {
+            valid = false
+            console.log("Zero division error.")
+        }
         result = number1 / number2
         break;
     case "*":
@@ -21,7 +58,8 @@ switch(operator) {
         break;
     default:
         console.log("Invalid")
+        valid = false;
         break;
 }
 
-console.log(result)
+if(valid) console.log(number1 , operator, number2, "=", result)
